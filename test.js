707c533c349966a3804715dae3337f2c42922012
shujiro <shujiro914@outlook.jp>
2022-06-19 18:58:22 +0900

    first commit

---
+++ test.js
@@ -0,0 +1,10 @@
+var text = "JavaScript";
+if (text == "HTML") {
+  console.log("条件Aの処理");
+  console.log(text);
+} else if (text == "JavaScript") {
+  console.log("条件Bの処理");
+  console.log(text);
+} else {
+  console.log("条件Aと条件B以外の処理");
+}
